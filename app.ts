b4412e8fa8e7498b655e06ac34ed47d54f4dc499
Koen Griffioen <koen.griffioen@capgemini.com>
2019-03-22 11:05:19 +0100

    Fixed missing CORS header

--- app.ts
+++ app.ts
@@ -4,6 +4,11 @@ const car = require('./routes/car.route'); // Imports routes for the products
 
 // initialize our express app
 const app = express();
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use('/cars', car);
 let port = 1234;
 
